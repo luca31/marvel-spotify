5164c1b8ebcf90a8d0db37a3ba5e29938aad3934
ellyromano03 <elisaromano0703@gmail.com>
2021-08-31 18:02:30 +0200

    boh

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,7 +1,10 @@
 function Playlist(props) {
   const songsList = props.songs.map((song, index) => {
     return(
-      <div key = {index}>{song.title}</div>
+      <div key = {index} className = {'playlist-element'}>
+        <span className = {'song-title'}>{song.title}</span>
+        <span className = {'song-author'}>{song.author}</span>
+      </div>
     )
   })
   return(
