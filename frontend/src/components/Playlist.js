8078c5e9f9b77f8eebcfa649c5895649f4ed19ae
ellyromano03 <elisaromano0703@gmail.com>
2021-09-09 12:40:23 +0200

    ciao

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -2,8 +2,11 @@ function Playlist(props) {
   const songsList = props.songs.map((song, index) => {
     return(
       <div key={index} className={'playlist-element'}>
-        <span className={'song-title'}>{song.title}</span>
-        <span className={'song-author'}>{song.author}</span>
+        <img src={'https://wallpapercave.com/wp/wp8269648.jpg'} alt={'song icon'} className={'song-icon'}></img>
+        <div className={'song-content'}>
+          <span className={'song-title'}>{song.title}</span>
+          <span className={'song-author'}>{song.author}</span>
+        </div>
       </div>
     )
   })
