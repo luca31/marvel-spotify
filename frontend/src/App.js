8d04d6e7e8cec59417dfb06fbb48905407aabf77
Luca Colli <lucacolli5@gmail.com>
2021-09-16 19:28:10 +0200

    cambiata libreria audio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import useSound from 'use-sound';
 import NavBar from './components/NavBar';
 import PlayingBar from './components/PlayingBar';
 import Home from './pages/Home';
@@ -12,27 +11,12 @@ import playlists from './playlists.json';
 function App() {
   const [ currentPlaylist, setCurrentPlaylist ] = React.useState(playlists[0]);
   const [ currentSong, setCurrentSong ] = React.useState(playlists[0].songs[0]);
-  const [ isPlaying, setIsPlaying ] = React.useState(false);
   const [ history, setHistory ] = React.useState([]);
   const [ page, changePage ] = React.useState('home');
 
-  const [play, { stop, pause, duration }] = useSound( '/tracks/' + currentSong.track + '-128k.mp3' );
-
   const changeSong = song => {
-    setIsPlaying(false);
-    stop();
     setCurrentSong(song);
     if(!history.includes(song)) setHistory(history.concat(song));
-  }
-
-  const switchMusicPlay = () => {
-    if(isPlaying) {
-      setIsPlaying(false);
-      pause();
-    } else {
-      setIsPlaying(true);
-      play();
-    }
   }
 
   return (
@@ -42,7 +26,7 @@ function App() {
         { page === 'home' ? <Home playlists={playlists} setCurrentSong={changeSong} currentPlaylist={currentPlaylist} setCurrentPlaylist={setCurrentPlaylist}></Home> : null }
         { page === 'history' ? <History history={history} setHistory={setHistory} setCurrentSong={changeSong} /> : null }
       </div>
-      <PlayingBar currentSong={currentSong} isPlaying={isPlaying} switchMusicPlay={switchMusicPlay} duration={duration} currentPlaylist={currentPlaylist} setCurrentSong={changeSong}></PlayingBar>
+      <PlayingBar currentSong={currentSong} currentPlaylist={currentPlaylist} setCurrentSong={changeSong}></PlayingBar>
     </>
   );
 }
--- frontend/src/components/PlayingBar.js
+++ frontend/src/components/PlayingBar.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Sound from 'react-sound';
+
+
 const formatTime = (ms) => {
   const minutes = Math.floor(ms / 60000);
   const seconds = ((ms % 60000) / 1000).toFixed(0);
@@ -9,11 +12,27 @@ const formatTime = (ms) => {
 }
 
 function PlayingBar(props) {
-  const [ duration, setDuration ] = React.useState();
+  const [ playingStatus, setPlayingStatus ] = React.useState(Sound.status.STOPPED);
+  const [ isPlaying, setIsPlaying ] = React.useState();
+  const [ duration, setDuration ] = React.useState(0);
+  const [ position, setPosition ] = React.useState(0);
+
+  const switchMusicPlay = () => {
+    if(playingStatus === Sound.status.PLAYING) {
+      setPlayingStatus(Sound.status.PAUSED);
+    } else {
+      setPlayingStatus(Sound.status.PLAYING);
+    }
+  }
 
   React.useEffect(() => {
-    setDuration(props.duration);
-  }, [props.duration] );
+    setIsPlaying(playingStatus === Sound.status.PLAYING ? true: false)
+  }, [ playingStatus ])
+
+  React.useEffect(() => {
+    setPosition(0);
+    setDuration(0)
+  }, [ props.currentSong ])
 
   const skipPrevious = () => {
     const currentIndex = props.currentPlaylist.songs.findIndex(e => e === props.currentSong);
@@ -22,6 +41,7 @@ function PlayingBar(props) {
     } else {
       props.setCurrentSong(props.currentPlaylist.songs[props.currentPlaylist.songs.length - 1]);
     }
+    setPlayingStatus(Sound.status.PLAYING);
   }
 
   const skipNext = () => {
@@ -31,26 +51,40 @@ function PlayingBar(props) {
     } else {
       props.setCurrentSong(props.currentPlaylist.songs[0]);
     }
+    setPlayingStatus(Sound.status.PLAYING);
   }
 
   return(
-    <div className={'playing-bar'}>
-      <div className={'playing-bar-content-start'}>
-        <img onClick={skipPrevious} src={'/icons/skip_previous_white_24dp.svg'} alt='skip-previous' />
-        { props.isPlaying ? <img onClick={props.switchMusicPlay} src={'/icons/pause_white_24dp.svg'} alt='pause' /> : <img onClick={props.switchMusicPlay} src={'/icons/play_arrow_white_24dp.svg'} alt='play' />}
-        <img onClick={skipNext} src={'/icons/skip_next_white_24dp.svg'} alt='skip-next' />
+    <>
+      <div className={'playing-bar'}>
+        <div className={'playing-bar-content-start'}>
+          <img onClick={skipPrevious} src={'/icons/skip_previous_white_24dp.svg'} alt='skip-previous' />
+          { isPlaying ?
+            <img onClick={switchMusicPlay} src={'/icons/pause_white_24dp.svg'} alt='pause' />
+            :
+            <img onClick={switchMusicPlay} src={'/icons/play_arrow_white_24dp.svg'} alt='play' />
+          }
+          <img onClick={skipNext} src={'/icons/skip_next_white_24dp.svg'} alt='skip-next' />
+        </div>
+        <div className={'playing-bar-content'}>
+          <span>{props.currentSong.title}</span>
+          <span className={'hide-on-mobile'}>&nbsp;-&nbsp;</span>
+          <span>{props.currentSong.author}</span>
+        </div>
+        <div className={'playing-bar-content-end'}>
+          <span>{formatTime(position)}</span>
+          <span className={'hide-on-mobile'}>&nbsp;/&nbsp;</span>
+          <span className={'hide-on-mobile'}>{formatTime(duration)}</span>
+        </div>
       </div>
-      <div className={'playing-bar-content'}>
-        <span>{props.currentSong.title}</span>
-        <span className={'hide-on-mobile'}>&nbsp;-&nbsp;</span>
-        <span>{props.currentSong.author}</span>
-      </div>
-      <div className={'playing-bar-content-end'}>
-        <span>0:00</span>
-        <span className={'hide-on-mobile'}>&nbsp;/&nbsp;</span>
-        <span className={'hide-on-mobile'}>{duration ? formatTime(duration) : null}</span>
-      </div>
-    </div>
+      <Sound
+        url={'/tracks/' + props.currentSong.track + '-128k.mp3'}
+        playStatus={playingStatus}
+        onLoading={({duration}) => setDuration(duration)}
+        onPlaying={({position}) => setPosition(position)}
+        onFinishedPlaying={skipNext}
+      />
+    </>
   );
 }
 
